d7fe691c66ee2f7c4794ed5aac5f967c2f0a1130
Lars Hupfeldt <lhn@hupfeldtit.dk>
2023-12-16 20:11:38 +0100

    Cleanup.

--- template/test/conftest.py
+++ template/test/conftest.py
@@ -12,19 +12,21 @@ _HERE = Path(__file__).absolute().parent
 
 _UNDERSCORE_TEST_RE = re.compile('_test$')
 
-def _test_key_shortener(request):
-    """Shorten test name while still keeping i unique"""
-    key_prefix = _UNDERSCORE_TEST_RE.sub('', request.node.module.__name__)
-    key_postfix = request.node.name.split('[')[0]
-    tfunc_prefix = key_prefix.replace('test.', 'test_') + '_'
+def _test_key_shortener(key_prefix, key_postfix):
+    tfunc_prefix = _UNDERSCORE_TEST_RE.sub('', key_prefix).replace('test.', 'test_') + '_'
     return key_prefix + '.' + key_postfix.replace(tfunc_prefix, '')
+
+
+def _test_node_shortener(request):
+    """Shorten test node name while still keeping it unique"""
+    return _test_key_shortener(request.node.module.__name__, request.node.name.split('[')[0])
 
 
 @fixture(name="out_dir")
 def _fixture_out_dir(request):
     """Create unique top level test directory for a test."""
 
-    out_dir = _HERE/'out'/_test_key_shortener(request)
+    out_dir = _HERE/'out'/_test_node_shortener(request)
 
     try:
         shutil.rmtree(out_dir)
